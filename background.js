4de2193315ad905cd33e2c528a5f6703695ae7b0
Kyle Neumeier <kneumei@gmail.com>
2015-03-31 22:58:29 -0500

    added icon

--- background.js
+++ background.js
@@ -3,9 +3,14 @@
  *
  * @see http://developer.chrome.com/apps/app.window.html
  */
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    id: 'main',
-    bounds: { width: 620, height: 500 }
-  });
+chrome.app.runtime.onLaunched.addListener(function () {
+	chrome.app.window.create('index.html', {
+		id: 'main',
+		bounds: {
+			width: 1280,
+			height: 1024
+		},
+		minWidth: 800,
+		minHeight: 600
+	});
 });
